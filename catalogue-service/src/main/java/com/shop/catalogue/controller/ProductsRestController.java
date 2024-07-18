40b311d7d8fc3f7e8c2116fd38e1237fbc9f1639
jokerSharp <qasoftware@tutanota.com>
2024-07-18 22:20:19 +0300

    add open api documentation for catalogue service

--- catalogue-service/src/main/java/com/shop/catalogue/controller/ProductsRestController.java
+++ catalogue-service/src/main/java/com/shop/catalogue/controller/ProductsRestController.java
@@ -3,8 +3,16 @@ package com.shop.catalogue.controller;
 import com.shop.catalogue.controller.payload.NewProductPayload;
 import com.shop.catalogue.entity.Product;
 import com.shop.catalogue.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.StringToClassMapItem;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
@@ -13,6 +21,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Map;
 
+@SecurityRequirement(name = "keycloak")
 @RestController
 @RequestMapping("catalogue-api/products")
 @RequiredArgsConstructor
@@ -27,6 +36,36 @@ public class ProductsRestController {
     }
 
     @PostMapping
+    @Operation(
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(
+                                    type = "object",
+                                    properties = {
+                                            @StringToClassMapItem(key = "title", value = String.class),
+                                            @StringToClassMapItem(key = "details", value = String.class)
+                                    }
+                            )
+                    )
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            headers = @Header(name = "Content-Type", description = "Data type"),
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(
+                                            type = "object",
+                                            properties = {
+                                                    @StringToClassMapItem(key = "id", value = Integer.class),
+                                                    @StringToClassMapItem(key = "title", value = String.class),
+                                                    @StringToClassMapItem(key = "details", value = String.class)
+                                            }
+                                    )
+                            )
+                    )
+            })
     public ResponseEntity<?> createProduct(@Valid @RequestBody NewProductPayload payload,
                                            BindingResult bindingResult,
                                            UriComponentsBuilder uriComponentsBuilder)
