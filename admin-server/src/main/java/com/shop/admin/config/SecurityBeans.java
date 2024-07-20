34817ad77cdbb79c1990953cd95b4c3d226ef9ac
jokerSharp <qasoftware@tutanota.com>
2024-07-20 23:18:43 +0300

    add monitoring for admin-service

--- admin-server/src/main/java/com/shop/admin/config/SecurityBeans.java
+++ admin-server/src/main/java/com/shop/admin/config/SecurityBeans.java
@@ -1,8 +1,10 @@
 package com.shop.admin.config;
 
 import com.shop.admin.web.client.OAuthHttpHeadersProvider;
+import jakarta.annotation.Priority;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -24,12 +26,31 @@ public class SecurityBeans {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Priority(0)
+    public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .oauth2Client(Customizer.withDefaults())
-                .authorizeHttpRequests(customizer -> customizer.anyRequest().permitAll())
+                .securityMatchers(customizer -> customizer
+                        .requestMatchers(HttpMethod.POST, "/instances")
+                        .requestMatchers(HttpMethod.DELETE, "/instances/*")
+                        .requestMatchers("/actuator/**"))
+                .oauth2ResourceServer(customizer -> customizer.jwt(Customizer.withDefaults()))
+                .authorizeHttpRequests(customizer -> customizer
+                        .requestMatchers("/instances", "/instances/*").hasAuthority("SCOPE_metrics_server")
+                        .requestMatchers("/actuator/**")
+                        .hasAuthority("SCOPE_metrics")
+                        .anyRequest().denyAll())
                 .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(CsrfConfigurer::disable)
+                .build();
+    }
+
+    @Bean
+    @Priority(1)
+    public SecurityFilterChain uiSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .oauth2Client(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults())
+                .authorizeHttpRequests(customizer -> customizer.anyRequest().authenticated())
                 .build();
     }
 }
